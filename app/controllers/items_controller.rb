d748d3cccdaa85ef6a636d953779c20f5b5a99f0
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-22 16:45:42 -0700

    flash, cookies, and session exercise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    # cookies[:hit_counter] = cookies[:hit_counter].to_i + 1
+    session[:hit_counter] = session[:hit_counter].to_i + 1
+    # binding.pry
+    if session[:hit_counter].to_i.even?
+      flash[:alert] = "Hit counter is even!"
+    else
+      flash[:message] = "Hit counter is odd!"
+    end
     @items = Item.all
   end
 
